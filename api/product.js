720f333427b761515559e782a00c1516a6b5c2bf
Lyertia-Senpai <lyertia@lyessa.live>
2021-08-20 16:40:44 +0300

    Update product.js

--- api/product.js
+++ api/product.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-app.set('views', path.join(__dirname, 'views'));
 router.get("/", async (req, res) => {
   res.render("hello.ejs")
 });
