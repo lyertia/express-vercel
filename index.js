59625a24b99052d7ce49f9de54251c999958df2d
Lyertia-Senpai <lyertia@lyessa.live>
2021-08-20 17:11:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
-
+app.get("/", async (req, res) => {
+  res.render("hello.ejs")
+});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
